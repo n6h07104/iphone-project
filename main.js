575e4f82f7c9127b1c18527f234d2ec15e994715
salem ahmed <146892672+n6h07104@users.noreply.github.com>
2023-10-30 01:56:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img =document.querySelector('.big-img');
+let body =document.querySelector("body");
+
+function phone(phone){
+    img.src = phone;
+}
+ 
+function colors(color){
+    body.style.background = color;
+}
